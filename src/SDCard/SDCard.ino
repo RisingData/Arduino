7ecdae7660f47ae305abb32c3699db75ddf21c90
Kevin John <notkevinjohn@gmail.com>
2016-10-18 16:32:57 -0700

    finished Arduino IDE Setup Guide, made wiring diagrams, started the SD card intergration docs

--- src/SDCard/SDCard.ino
+++ src/SDCard/SDCard.ino
@@ -1,58 +1,30 @@
-// include the SD library:
 #include <SPI.h>
 #include <SD.h>
-
-Sd2Card card;
-SdVolume volume;
-SdFile root;
 
 const int chipSelect = 10;
 
 void setup() {
-  // Open serial communications and wait for port to open:
-  Serial.begin(115200);
-  //while (!Serial) {
-  //  ; // wait for serial port to connect. Needed for native USB port only
-  //}
-
-
+  Serial.begin(115200); 
   Serial.print("Initializing SD card...");
-
-  // see if the card is present and can be initialized:
+ 
   if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    return;
+    Serial.println("Card failed, or not present");   
+    while(1);
   }
   Serial.println("card initialized.");
 }
 
 void loop() {
-  // make a string for assembling the data to log:
-  String dataString = "";
-
-  // read three sensors and append to the string:
-  for (int analogPin = 0; analogPin < 3; analogPin++) {
-    int sensor = analogRead(analogPin);
-    dataString += String(sensor);
-    if (analogPin < 2) {
-      dataString += ",";
-    }
-  }
-
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
-
-  // if the file is available, write to it:
+  
+  File dataFile = SD.open("myFile.txt", FILE_WRITE);
+  String dataString = "Hello World";
   if (dataFile) {
     dataFile.println(dataString);
-    dataFile.close();
-    // print to the serial port too:
+    dataFile.close();   
     Serial.println(dataString);
-  }
-  // if the file isn't open, pop up an error:
+  } 
   else {
-    Serial.println("error opening datalog.txt");
+    Serial.println("error opening file");
   }
+  delay(100);
 }
